f41b120693c46084bbf6dad9f2b98113aa829a7d
张一博 <yibo.zhang@zymobi.com>
2019-10-18 15:34:42 +0800

    更新极光SDK

---
+++ src/org/zywx/wbpalmstar/widgetone/uexjpush/receiver/PushMessageReceiver.java
@@ -0,0 +1,7 @@
+package org.zywx.wbpalmstar.widgetone.uexjpush.receiver;
+
+import cn.jpush.android.service.JPushMessageReceiver;
+
+public class PushMessageReceiver extends JPushMessageReceiver {
+
+}
